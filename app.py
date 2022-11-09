bf49673c53bebf64f356356344d2ad97d1189f38
theossalmeida <theoalmeida00@gmail.com>
2022-11-09 19:51:54 -0300

    v2

--- app.py
+++ app.py
@@ -1,18 +1,19 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow
 import design
-from backend import puxa_dados, normalizacao
-from pyqtgraph import PlotWidget, plot
 import pandas as pd
 from pandas_datareader import data as wb
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 
-
+dados = pd.DataFrame()
 acoes = []
 data_usuario_inicio = []
 data_usuario_final = []
 acao = ""
+
 
 class GeraGrafico(QMainWindow, design.Ui_displayGrafico):
     def __init__(self, parent=None):
@@ -31,8 +32,8 @@ class GeraGrafico(QMainWindow, design.Ui_displayGrafico):
         return data_usuario_inicio
 
     def add_data_final(self):
-            data_usuario_final.append(self.recebeDataFinal.text())
-            return data_usuario_final
+        data_usuario_final.append(self.recebeDataFinal.text())
+        return str(data_usuario_final)
 
     def add_lista(self):
         acao = self.recebeAcao.text()
@@ -49,16 +50,34 @@ class GeraGrafico(QMainWindow, design.Ui_displayGrafico):
         )
 
     def gera_grafico(self):
-        self.grafico.show(
-            puxa_dados(acoes, data_usuario_inicio[len(data_usuario_inicio)-1], 
-            data_usuario_final[len(data_usuario_final)-1])
-        )
+        self.figure.clear()
+        if len(data_usuario_final) != 0:
+            for i in acoes:
+                dados[i] = wb.DataReader(i, data_source="yahoo", start=data_usuario_inicio[len(data_usuario_inicio)-1],
+                                         end=data_usuario_final[len(data_usuario_final)-1])['Adj Close']
+        else:
+            for i in acoes:
+                dados[i] = wb.DataReader(i, data_source="yahoo", start=data_usuario_inicio[len(
+                    data_usuario_inicio)-1])['Adj Close']
+
+        plt.plot(dados)
+        plt.legend(dados)
+        self.canvas.draw()
 
     def normaliza(self):
-        self.grafico.show(
-            normalizacao(acoes, data_usuario_inicio[len(data_usuario_inicio)-1], 
-            data_usuario_final[len(data_usuario_final)-1])
-        )
+        self.figure.clear()
+        if len(data_usuario_final) != 0:
+            for i in acoes:
+                dados[i] = wb.DataReader(i, data_source="yahoo", start=data_usuario_inicio[len(data_usuario_inicio)-1],
+                                         end=data_usuario_final[len(data_usuario_final)-1])['Adj Close']
+        else:
+            for i in acoes:
+                dados[i] = wb.DataReader(i, data_source="yahoo", start=data_usuario_inicio[len(
+                    data_usuario_inicio)-1])['Adj Close']
+
+        plt.plot((dados / dados.iloc[0]))
+        plt.legend(dados)
+        self.canvas.draw()
 
 
 if __name__ == '__main__':
